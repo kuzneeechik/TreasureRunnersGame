cd39a1e2e4675b6341a9d309f7ff7838e0b8bf80
Vasilina Kuznetsova <vasilina.kuznecova2@gmail.com>
2025-06-24 01:42:23 +0700

    fix level 3

--- TreasureRunnersGame/Assets/Script/DoubleLever.cs
+++ TreasureRunnersGame/Assets/Script/DoubleLever.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class L3_2Lever : MonoBehaviour
+public class DoubleLever : MonoBehaviour
 {
-    private int tapCount = 0; // Счетчик нажатий (0 - не нажато, 1 - открыли, 2 - закрыли)
+    private int tapCount = 0;
     private GameObject playerInRange = null;
     private Animator animator;
     public Door door;
@@ -14,7 +14,6 @@ public class L3_2Lever : MonoBehaviour
 
     private void Update()
     {
-        // Если уже сделали 2 действия (открыли и закрыли) - выходим
         if (tapCount >= 2 || playerInRange == null) return;
 
         string tag = playerInRange.tag;
@@ -30,13 +29,11 @@ public class L3_2Lever : MonoBehaviour
     {
         tapCount++;
 
-        // Первое нажатие - открываем дверь
         if (tapCount == 1)
         {
             animator.SetBool("isTapped", true);
             door.animator.SetBool("isOpen", true);
         }
-        // Второе нажатие - закрываем дверь
         else if (tapCount == 2)
         {
             animator.SetBool("isTapped", false);
