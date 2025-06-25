8a6771fec1409c022aad26d34daf087be282e4f5
ElizavetaDorofeeva <Lizaador@mail.ru>
2025-06-25 18:07:44 +0700

    add realization

--- TreasureRunnersGame/Assets/Script/ArtefactParentManager.cs
+++ TreasureRunnersGame/Assets/Script/ArtefactParentManager.cs
@@ -4,18 +4,34 @@ public class ArtefactParentManager : MonoBehaviour
 {
     private void Start()
     {
-        if (SaveLevel.Instance == null)
-        {
-            return;
-        }
+        if (SaveLevel.Instance == null) return;
 
         foreach (Transform child in transform)
         {
             Artefact_ID artefact = child.GetComponent<Artefact_ID>();
-            if (artefact != null)
+            if (artefact == null) continue;
+
+            bool shouldBeActive = SaveLevel.Instance.Artifacts.Contains(artefact.Id);
+
+            child.gameObject.SetActive(shouldBeActive);
+
+            if (artefact.StoredObject != null)
             {
-                bool shouldBeActive = SaveLevel.Instance.Artifacts.Contains(artefact.Id);
-                child.gameObject.SetActive(shouldBeActive);
+                GameObject targetObject = null;
+
+                if (artefact.StoredObject is GameObject gameObj)
+                {
+                    targetObject = gameObj;
+                }
+                else if (artefact.StoredObject is Component component)
+                {
+                    targetObject = component.gameObject;
+                }
+
+                if (targetObject != null)
+                {
+                    targetObject.SetActive(!shouldBeActive);
+                }
             }
         }
     }
